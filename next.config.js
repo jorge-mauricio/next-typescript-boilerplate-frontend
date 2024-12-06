4bade40ce689815bdcc4e60cbfbfd1c3db18aab2
jm-fullstack <contact@fullstackwebdesigner.com>
2024-12-06 09:55:27 -0300

    build: next basic configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  // Optional: Add other commonly used options
+  poweredByHeader: false,
+  compress: true,
+};
+
+module.exports = nextConfig;
