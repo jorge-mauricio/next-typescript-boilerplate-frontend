f10b11bda9eecacf95d143f0b2769f8203a0aa2b
jm-fullstack <contact@fullstackwebdesigner.com>
2024-12-06 09:57:23 -0300

    build: tailwind extension and global css enable

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,102 +1,111 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  // parserOptions: {
-  //   // project: './tsconfig.json',
-  //   // project: './react-next-typescript-frontend/tsconfig.json',
-  //   project: 'tsconfig.json',
-  //   ecmaVersion: 2020,
-  //   sourceType: 'module',
-  //   ecmaFeatures: {
-  //     jsx: true,
-  //   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["./tsconfig.json"], // Notice the array syntax
-    tsconfigRootDir: __dirname, // Add this line
+    project: ['./tsconfig.json'], // Notice the array syntax
+    tsconfigRootDir: __dirname,   // Add this line
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
   extends: [
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended", // Prettier must be last to override other configs
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended'  // Prettier must be last to override other configs
   ],
-  plugins: ["@typescript-eslint", "react", "prettier", "import"],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'import'
+  ],
   rules: {
     // Disable the problematic rules
-    "@typescript-eslint/lines-between-class-members": "off",
-    "@typescript-eslint/no-throw-literal": "off",
+    '@typescript-eslint/lines-between-class-members': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
 
     // Next.js specific rules
-    "react/react-in-jsx-scope": "off",
-    "react/require-default-props": "off",
-    "react/jsx-props-no-spreading": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
 
     // TypeScript specific rules
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }],
 
     // Project preferences
-    "import/prefer-default-export": "off",
-    "import/order": [
-      "error",
+    'import/prefer-default-export': 'off',
+    'import/order': [
+      'error',
       {
         groups: [
-          "builtin",
-          "external",
-          "internal",
-          ["parent", "sibling"],
-          "index",
-          "object",
-          "type",
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+          'type'
         ],
-        "newlines-between": "always",
+        'newlines-between': 'always',
         alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-      },
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
     ],
 
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
-    "no-console": ["warn", { allow: ["warn", "error"] }],
-
-    "react/function-component-definition": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        namedComponents: "arrow-function",
-        unnamedComponents: "arrow-function",
+        "printWidth": 100,
+        "jsxBracketSameLine": false,
+
+        'htmlWhitespaceSensitivity': 'css',
+        'proseWrap': 'preserve'
       },
+      { usePrettierrc: true }
     ],
-    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.tsx'] }
+    ]
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
-        alwaysTryTypes: true,
-      },
+        alwaysTryTypes: true
+      }
     },
     react: {
-      version: "detect",
-    },
+      version: 'detect'
+    }
   },
+  // Ignore the following files (parse as js)
   ignorePatterns: [
-    ".eslintrc.js",
-    "next.config.js",
-    "node_modules",
-    ".next",
-    "out",
-  ],
+    '.eslintrc.js',
+    'next.config.js',
+    'postcss.config.js',
+    'tailwind.config.js',
+    'node_modules',
+    '.next',
+    'out'
+  ]
 };
