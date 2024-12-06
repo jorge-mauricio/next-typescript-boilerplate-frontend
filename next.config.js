dd9540c38610bbf5f4c47e1bc121db7459643ed2
jm-fullstack <contact@fullstackwebdesigner.com>
2024-12-06 10:11:53 -0300

    feat: next config sass

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   // Optional: Add other commonly used options
   poweredByHeader: false,
   compress: true,
+  sassOptions: {
+    includePaths: ['./src'],
+  },
 };
 
 module.exports = nextConfig;
