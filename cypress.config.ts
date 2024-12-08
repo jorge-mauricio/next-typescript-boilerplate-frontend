3ccd9e94cad5024058506f4cb9a9d08cd64e8111
Jorge Mauricio <web@jorgemauricio.com>
2024-12-08 14:00:21 -0300

    fix: cypress configuration adjustment to run with typescript

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,12 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
     },
   },
   viewportWidth: 1280,
